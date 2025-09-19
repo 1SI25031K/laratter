d49513570cb22fcb246af30e05497c98e81328e4
Kosei M <miyamoto.kosei.698@s.kyushu-u.ac.jp>
2025-09-19 16:52:40 +0900

    コミットメッセージ

--- app/Http/Controllers/TweetLikeController.php
+++ app/Http/Controllers/TweetLikeController.php
@@ -28,7 +28,7 @@ class TweetLikeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Tweet $tweet)
     {
         $tweet->liked()->attach(auth()->id());
         return back();
@@ -61,7 +61,7 @@ class TweetLikeController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Tweet $tweet)
     {
         $tweet->liked()->detach(auth()->id());
         return back();
